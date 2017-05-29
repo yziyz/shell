73b98c50b3cf1a42cb645e440513464d2d493d94
yuanzhen <yziyz@foxmail.com>
2017-05-29 13:39:42 +0800

    Update redis version from 3.2.8 to 3.2.9

--- install/centos7-install-redis.sh
+++ install/centos7-install-redis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Redis 3.2.8 installation script.
-wget -O ~/redis-3.2.8.tar.gz http://download.redis.io/releases/redis-3.2.8.tar.gz
-sudo tar -zxf ~/redis-3.2.8.tar.gz --directory=/usr/local
-sudo mv /usr/local/redis-3.2.8 /usr/local/redis
+# Redis 3.2.9 installation script.
+wget -O ~/redis-3.2.9.tar.gz http://download.redis.io/releases/redis-3.2.9.tar.gz
+sudo tar -zxf ~/redis-3.2.9.tar.gz --directory=/usr/local
+sudo mv /usr/local/redis-3.2.9 /usr/local/redis
 cd /usr/local/redis
 sudo make
 /usr/local/redis/src/redis-server --version
